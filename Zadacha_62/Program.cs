4a9dbbe06730c8d6e02f60960a91312392f28521
elizavetanikolaenko2017 <elizaveta.nikolaenko2017@gmail.com>
2023-03-26 02:09:06 +0300

    Задача №62

--- Zadacha_62/Program.cs
+++ Zadacha_62/Program.cs
@@ -7,22 +7,44 @@
 10 09 08 07
 */
 
-int ReadInt(string text)       /////1-   задаем тип числа
+
+ int n=4;
+ int[,]  sqareMatrix= new int[n,n];
+
+int temp=1;
+int i=0;
+int j=0;
+
+while (temp<= sqareMatrix.GetLength(0)* sqareMatrix.GetLength(1))
 {
- Console.Write(text);
- return Convert.ToInt32(Console.ReadLine());
+  sqareMatrix[i,j]= temp;
+  temp++;
+  if (i<=j+1 && i+j <sqareMatrix.GetLength(1)-1)
+  j++;
+  else if(i<j && i+j >= sqareMatrix.GetLength(0)-1)
+  i++;
+  else if(i>=j && i+j> sqareMatrix.GetLength(1)-1)
+  j--;
+  else
+  i--;
+  
+   
+  
 }
 
-int[,]GenerateMatrix(int m, int n)     /// 2  создаем шаблон матрицы
-{ 
-   int[,]matrix=new int[m,n];
-   Random rand=new Random();
-   for (int i = 0; i < matrix.GetLength(0); i++)
+WriteArray(sqareMatrix);
+
+void WriteArray(int[,]array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
    {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-       matrix[i,j]=new Random().Next(1,10); 
-    }
-   } 
-   return matrix;
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+         if(array[i,j]/10 <= 0)
+         Console.Write($"{array[i,j]} ");
+
+         else System.Console.Write($"{array[i,j]}");
+      }
+      System.Console.WriteLine();
+   }
 }
