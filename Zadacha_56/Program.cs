c0ff6e0320477031c83c8c6a1ae6c2160ec91695
elizavetanikolaenko2017 <elizaveta.nikolaenko2017@gmail.com>
2023-03-26 00:41:17 +0300

    Задача №56

--- Zadacha_56/Program.cs
+++ Zadacha_56/Program.cs
@@ -10,63 +10,64 @@
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
 
+int m=InputNumbers("Введите количество строк: ");
+int n=InputNumbers("Введите количество столбцов: ");
 
-int ReadInt(string text)       /////1-   задаем тип числа
+int[,]array=new int[m,n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine=0;
+int sumLine=sumLineElements(array,0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
- Console.Write(text);
- return Convert.ToInt32(Console.ReadLine());
-}
-
-int[,]GenerateMatrix(int m, int n)     /// 2  создаем шаблон матрицы
-{ 
-   int[,]matrix=new int[m,n];
-   Random rand=new Random();
-   for (int i = 0; i < matrix.GetLength(0); i++)
+   int tempSumLine=sumLineElements(array,i);
+   if (sumLine>tempSumLine)
    {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-       matrix[i,j]=new Random().Next(1,10); 
-    }
-   } 
-   return matrix;
+      sumLine=tempSumLine;
+      minSumLine=i;
+   }
+   
 }
+Console.WriteLine($"\n{minSumLine+1} - строка с наименьшей суммой ({sumLine}) элементов");
 
-void NewSumMatrix(int[,]matrix)
-{ 
- for (int i = 0; i < matrix.GetLength(0); i++)
- {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-      int SumminString=0;
-      int minSum=SumminString(matrix,0);
-      for (int i = 1; i < matrix.GetLength(0); i++)
-      {
-        int tempSumString=SumminString(matrix,i);
-        if (Summin>tempSumString)
-        {
-            Summin=tempSumString;
-            minSum=i;
-        }
-      }  
-    }
- }
-}
-Console.WriteLine($"{SumMinSring+1} - строка с наименьшей суммой элементов");
-
-void PrintSumMinString(int[,]matrix)
+int sumLineElements(int[,]array, int i)
 {
- for (int i = 0; i < matrix.GetLength(0); i++)
- {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        matrix[i,j]=new Random().Next(1,10);
-    }
- }
- Console.WriteLine();
+   int sumLine=array[i,0];
+   for (int j = 0; j <array.GetLength(1); j++)
+   {
+      sumLine+=array[i,j];
+   }
+   return sumLine;
 }
 
-int m=ReadInt("Введите количество строк: ");
-int n=ReadInt("Введите количество столбцов: ");
-var myMatrix=GenerateMatrix(m,n);
-NewSumMatrix(matrix);
-PrintSumMinString(matrix);
+int InputNumbers(string text)
+{
+   Console.Write(text);
+   int output=Convert.ToInt32(Console.ReadLine());
+   return output;
+}
+
+void CreateArray(int[,]array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j= 0; j < array.GetLength(1); i++)
+      {
+         Console.Write(array[i,j]+ " ");
+      }
+      Console.WriteLine();
+   }
+}
+
+void WriteArray(int[,]array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+     Console.Write(array[i,j]+ " "); 
+   }
+   Console.WriteLine();
+ }
+}
